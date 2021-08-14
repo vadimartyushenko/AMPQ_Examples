4f6bd0d967d85d14d1f59177a5ab2b5ad25c19a6
Вадим Артюшенко <artushenkomail@mail.ru>
2021-08-14 17:14:41 +0700

    Добавьте файлы проекта.

---
+++ Publisher/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Publisher
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var factory = new ConnectionFactory() {HostName = "localhost"};
+
+			using (var connection = factory.CreateConnection())
+			using (var channel = connection.CreateModel())
+			{
+				channel.QueueDeclare(queue: "FirstQueue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+				
+				string message = "Message from publisher";
+
+				var body = Encoding.UTF8.GetBytes(message);
+				channel.BasicPublish("", "FirstQueue", null, body);
+
+				Console.WriteLine("Message is sent to Default Exchange");
+			}
+		}
+	}
+}
