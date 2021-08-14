519233f9c13af240db5ca5ee615c8a6dfa809e9d
Вадим Артюшенко <artushenkomail@mail.ru>
2021-08-14 17:42:45 +0700

    feature: простая реализация для потнребителя

--- Consumer/Program.cs
+++ Consumer/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+using RabbitMQ.Client.Events;
 
 namespace Consumer
 {
@@ -6,7 +9,28 @@ namespace Consumer
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			var factory = new ConnectionFactory() { HostName = "localhost" };
+			var queueName = "FirstQueue";
+
+			using (var connection = factory.CreateConnection())
+			using (var channel = connection.CreateModel())
+			{
+				channel.QueueDeclare(queue: queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+				var consumer = new EventingBasicConsumer(channel);
+
+				consumer.Received += (sender, e) =>
+				{
+					var body = e.Body;
+					var message = Encoding.UTF8.GetString(body.ToArray());
+					Console.WriteLine(" Received message: {0}", message);
+				};
+
+				channel.BasicConsume(queueName, true, consumer);
+
+				Console.WriteLine("Subscribed to the queue '{0}'", queueName);
+			}
 		}
+
 	}
 }
