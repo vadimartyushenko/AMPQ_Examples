dcb6265fdf8fdf0c4effc8e15f55da844580fc31
Vadim Artyshenko <artushenkomail@mail.ru>
2023-08-23 18:43:33 +0700

    [feature] (ProductAPI): init asp.net project for producer API

---
+++ ProductAPI/Data/ProductDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductAPI.Models;
+
+namespace ProductAPI.Data;
+
+public class ProductDbContext : DbContext
+{
+    private readonly IConfiguration _configuration;
+
+    public ProductDbContext(IConfiguration configuration) => _configuration = configuration;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+        optionsBuilder.UseSqlServer(_configuration.GetConnectionString("DefaultConnection"));
+    
+    public DbSet<Product> Products { get; set; }
+}
---
+++ ProductAPI/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductAPI.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int Price { get; set; }
+    public int Stock { get; set; }
+}
