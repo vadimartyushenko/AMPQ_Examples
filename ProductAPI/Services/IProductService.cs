8811f57b206150fd87b4dfd07da17a13da521513
Vadim Artyshenko <artushenkomail@mail.ru>
2023-08-24 17:42:47 +0700

    [feature] (ProductAPI): added migrations + product service

---
+++ ProductAPI/Services/IProductService.cs
@@ -0,0 +1,16 @@
+﻿using ProductAPI.Models;
+
+namespace ProductAPI.Services;
+
+public interface IProductService
+{
+    public IEnumerable<Product> GetAllProducts();
+    
+    public Product GetProductById(int id);
+
+    public Product AddProduct(Product product);
+    
+    public Product UpdateProduct(Product product);
+
+    public bool DeleteProductById(int id);
+}
---
+++ ProductAPI/Services/ProductService.cs
@@ -0,0 +1,47 @@
+﻿using ProductAPI.Data;
+using ProductAPI.Models;
+
+namespace ProductAPI.Services;
+
+public class ProductService : IProductService
+{
+    private readonly ProductDbContext _dbContext;
+    
+    public ProductService(ProductDbContext dbContext) => _dbContext = dbContext;
+
+    public IEnumerable<Product> GetAllProducts()
+    {
+        return _dbContext.Products.ToList();
+    }
+
+    public Product GetProductById(int id)
+    {
+       return _dbContext.Products.FirstOrDefault(x => x.Id == id);
+    }
+
+    public Product AddProduct(Product product)
+    {
+        var result = _dbContext.Products.Add(product);
+        _dbContext.SaveChanges();
+        return result.Entity;
+    }
+
+    public Product UpdateProduct(Product product)
+    {
+        var result = _dbContext.Products.Update(product);
+        _dbContext.SaveChanges();
+        return result.Entity;
+    }
+
+    public bool DeleteProductById(int id)
+    {
+        var filtered = _dbContext.Products.FirstOrDefault(x => x.Id == id);
+        if (filtered != null) {
+            var result = _dbContext.Remove(filtered);
+            _dbContext.SaveChanges();
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
