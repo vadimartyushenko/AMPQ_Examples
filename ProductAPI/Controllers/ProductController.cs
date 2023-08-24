a1898f89fbaded6613cbfe18b1e1a1ccfd10084e
Vadim Artyshenko <artushenkomail@mail.ru>
2023-08-24 19:14:34 +0700

    added ProductController

---
+++ ProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductAPI.Models;
+using ProductAPI.Services;
+
+namespace ProductAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductController : ControllerBase
+{
+    private readonly IProductService _productService;
+    public ProductController(IProductService productService)
+    {
+        _productService = productService;
+    }
+
+    [HttpGet("products")]
+    public IEnumerable<Product> Products()
+    {
+        return _productService.GetAllProducts();
+    }
+}
